37efb630ac4c1884baef0ad3f5149ae668a63fcf
Joohoon Kim <jayjhkim7@gmail.com>
2021-04-03 11:09:57 -0400

    added json-server

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -9,6 +9,13 @@ const {
   GraphQLList,
 } = graphql;
 //GraphQLSchema is a helper that takes in a RootQuery and returns a GraphQL instance
+//graphiql is the client and the graphql-express is its server at 5000
+//GraphQL server(at 5000) makes queries to outside 3rd party server(3000) that has its own database to grad data from it - Sct3.14)
+//json-server is used here to mock that 3rd party server. it needs infomration in db.json file base on which it mocks that server
+//json-server is a completely separate process from the express-graphql server running
+//json-server is run on 3000 by default (https://www.npmjs.com/package/json-server#alternative-port)
+//to make json-server run on an alternative port use --port flag ($ json-server --watch db.json --port 3004)
+//so both servers need to be running
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
@@ -19,8 +26,10 @@ const CompanyType = new GraphQLObjectType({
     users: {
       type: new GraphQLList(UserType),
       async resolve(parentValue, args) {
+        //the needed data is nested as a value to the key 'data' in axios response. difference with fetch api. this is just how axios was designed
         const { data } = await axios.get(
-          `http://localhost:3000/companies/${parentValue.id}/users`
+          `http://localhost:3004/companies/${parentValue.id}/users`
+          //request from graphQl server at 5000 over to remote json-server at 3004
         );
         return data;
       },
@@ -39,7 +48,7 @@ const UserType = new GraphQLObjectType({
       type: CompanyType,
       async resolve(parentValue, args) {
         const { data } = await axios.get(
-          `http://localhost:3000/companies/${parentValue.companyId}`
+          `http://localhost:3004/companies/${parentValue.companyId}`
         );
         return data;
       },
@@ -56,10 +65,12 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       async resolve(parentValue, args) {
         const { data } = await axios.get(
-          `http://localhost:3000/users/${args.id}`
+          `http://localhost:3004/users/${args.id}`
         );
+        //args is short for arguments. signifies arguments required for the RootQuery for a given user
         //restructuring data was done because axios returns (when promise resolves) something that looks like '{data: {...}, status: 200, headers: {...} ...}'
         return data;
+        //Root query is used to allow graphQL to enter into the application's data graph
         //If my query expects to be provided with the id of the user it is fetching, that id will be present in the args object that has been used as the second parameter here in resolve function
       },
     },
@@ -68,7 +79,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       async resolve(parentValue, args) {
         const { data } = await axios.get(
-          `http://localhost:3000/companies/${args.id}`
+          `http://localhost:3004/companies/${args.id}`
         );
         return data;
       },
--- users/server.js
+++ users/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+//glue layer(compatibility layer) between express and graphQL
 const expressGraphQL = require('express-graphql');
 const schema = require('./schema/schema');
 const app = express();
+const port = 5000;
 
 //Antyime a request comes in with this endpoint, have graphQL library handle it
+//expressGraphQL is used here as a middleware hence app.use
 app.use('/graphql', expressGraphQL.graphqlHTTP({ schema, graphiql: true }));
 
-app.listen(4000, () => {
-  console.log('Express server listening');
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
 });
